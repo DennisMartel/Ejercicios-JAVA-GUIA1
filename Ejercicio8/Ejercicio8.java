c5903d713515136a9ae82144b0992146cc319d94
Dennis Martel <marteldennis05@gmail.com>
2023-02-08 07:11:27 -0600

    Ejercicio8

--- Ejercicio8/Ejercicio8.java
+++ Ejercicio8/Ejercicio8.java
@@ -6,10 +6,19 @@ public class Ejercicio8
     public static void main(String args[]) 
     {
         Scanner scanner = new Scanner(System.in);
-        int impares;
+        int cantidadProductos = 0;
+        int totalPagar = 0;
 
         try {
-            
+            System.out.println("Ingresa la cantidad de productos");
+            cantidadProductos = scanner.nextInt();
+
+            for (int i = 1; i <= cantidadProductos; i++) {
+                System.out.println("Ingresa la cantidad a pagar del producto "+i);
+                totalPagar += scanner.nextInt();
+            }
+
+            System.out.println("El total a pagar por los "+cantidadProductos+" productos es: "+totalPagar);
         } finally {
             scanner.close();
         }
