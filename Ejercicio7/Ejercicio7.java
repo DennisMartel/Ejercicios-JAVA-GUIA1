7caa14d685e3e451d4e5dbe87b8fcc0654bd29f3
Dennis Martel <marteldennis05@gmail.com>
2023-02-11 13:31:27 -0600

    Ejercicio7

--- Ejercicio7/Ejercicio7.java
+++ Ejercicio7/Ejercicio7.java
@@ -6,10 +6,24 @@ public class Ejercicio7
     public static void main(String args[]) 
     {
         Scanner scanner = new Scanner(System.in);
-        int impares;
+        int numero = 0;
+        int contador = 0;
 
         try {
-            
+            System.out.println("Ingresa un número");
+            numero = scanner.nextInt();
+
+            for (int i = 1; i <= numero; i++) {
+                if (numero % i == 0) {
+                    contador++;
+                }
+            }
+
+            if (contador == 2) {
+                System.out.println(numero+" es un número primo");
+            } else {
+                System.out.println(numero+" no es un número primo");
+            }
         } finally {
             scanner.close();
         }
