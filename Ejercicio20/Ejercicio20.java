88362306357dd6c9aabad5c3561a1ec4ef8f9c7f
Dennis Martel <marteldennis05@gmail.com>
2023-02-18 18:43:45 -0600

    Ejercicio19 y Ejercicio20

---
+++ Ejercicio20/Ejercicio20.java
@@ -0,0 +1,46 @@
+// Hacer un algoritmo que sume las parte 1 
+// (todos los elementos que seencuentra en la parte inferior de la diagonal principal). 
+// Según la matrizdel problema 18
+import java.util.Scanner;
+
+public class Ejercicio20
+{
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int value = 1;
+        System.out.println("ingresar los valores de la matriz");
+        int n = scanner.nextInt();
+        int matriz[][] = new int[n][n];
+        int suma = 0;
+
+        try {
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    matriz[i][j] = value;
+                    value++;
+                }
+            }  
+
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.print("\t"+ matriz[i][j]);
+                }
+                System.out.println("");
+            }   
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (j < i) {
+                        suma = suma + matriz[i][j];
+                    }
+                }
+            }
+            System.out.println("\n"+"El resultado de la suma de los elementos que se");
+            System.out.println("encuentran en la parte inferior de la diagonal es: "+suma);
+        } finally {
+            scanner.close();
+        }
+    }
+}
